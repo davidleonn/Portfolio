31e1037956ad68a54bd7fb19523504e5ba407e57
david <davidleon_06@hotmail.com>
2023-02-01 11:40:05 +0100

    Setup + navbar start

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <select className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </select>
+    <div>
+      <h1 className="text-3xl ">Hello</h1>
+    </div>
   );
 }
 
